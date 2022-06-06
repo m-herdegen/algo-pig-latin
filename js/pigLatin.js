17033b51f85981dcc088a1f406d83375764b09d8
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-06 15:31:42 -0400

    python and js working

--- js/pigLatin.js
+++ js/pigLatin.js
@@ -1,2 +1,47 @@
 exports.translate = function(word) {
+    const vowels = 'AEIOUaeiou'
+    let ansString = ''
+    const workingArray = word.split(' ')
+    const added = 'ay'
+
+    for (i = 0; i < workingArray.length; i++){
+        if (workingArray[i].slice(0,2) == 'qu'){
+            ansString += workingArray[i].slice(2,)
+            ansString += workingArray[i].slice(0,2)
+            ansString += added
+        }
+        else if (vowels.includes(workingArray[i][0])){
+            ansString += workingArray[i]
+            ansString += added
+        }
+        else if (vowels.includes(workingArray[i][1])){
+            ansString += workingArray[i].slice(1,)
+            ansString += workingArray[i][0]
+            ansString += added
+        }
+        else if (vowels.includes(workingArray[i][2])){
+            if (workingArray[i].slice(1,3) == 'qu'){
+                ansString += workingArray[i].slice(3,)
+                ansString += workingArray[i].slice(0, 3)
+                ansString += added
+            }
+            else {
+                ansString += workingArray[i].slice(2,)
+                ansString += workingArray[i].slice(0,2)
+                ansString += added
+            }
+        }
+        else if (vowels.includes(workingArray[i][3])){
+            ansString += workingArray[i].slice(3,)
+            ansString += workingArray[i].slice(0,3)
+            ansString += added
+        }
+        
+        if (i < workingArray.length -1){
+            ansString += ' '
+        }
+    }   
+
+    console.log(ansString)
+    return ansString
 };
